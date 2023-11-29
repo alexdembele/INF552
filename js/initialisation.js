355032abcb4c3285579cd0c3d7ea5e0bd5dd0c67
alexdembele <chessdembel@gmail.com>
2023-11-29 23:05:04 +0100

    carte colorée

--- js/initialisation.js
+++ js/initialisation.js
@@ -1,11 +1,23 @@
 const ctx = { 
     width: 960,
-    height:540
+    height:540,
+    date:2000
+
 };
 
 function createViz() {
     console.log("Using D3 v" + d3.version);
     
+    CreateMap();
+    LoadData();
+
+
+
+
+};
+
+function CreateMap()
+{
     // Création de la projection géographique
     var projection = d3.geoMercator()
     .scale(120)
@@ -18,21 +30,61 @@ function createViz() {
     .attr("width", ctx.width)
     .attr("height", ctx.height);
 
-    // Charge les données géographiques
+    
     d3.json("data/country-110m.json").then(function (world) {
-    // Ajoute les pays à la carte
-    console.log(topojson.feature(world, world.objects.countries).features)
+    //Construction carte
     svg.selectAll("path")
     .data(topojson.feature(world, world.objects.countries).features)
     .enter().append("path")
     .attr("d", path)
+    //gestion souris
     .on("click", function (event,d) {
-        // Gestion de l'événement de clic
-
         console.log("Pays cliqué :", d.properties.name)
+    })
+    .on("mouseover", function (d) {
+        d3.select(this).style("opacity", 0.5);
+    })
+    .on("mouseout", function (d) {
+        d3.select(this).style("opacity",1) 
+        
     });
 });
-
-
 };
 
+function LoadData()
+{
+    d3.csv("data/sdg_index_2000-2022.csv").then(function (data) {ColorMap(data)}).catch(function (err) { console.log(err); });
+};
+
+function ColorMap(data)
+{
+    let sdgColorScale = d3.scaleLinear()
+    .domain([0, 50, 100]) // Les points de référence pour les couleurs
+    .range(["red", "white", "green"]) // Les couleurs correspondantes
+    //Colore la map avec le sdg_index_score
+    let paysIndex={}
+    
+    data.forEach(element => {
+        if(element.year==2000)
+        {
+            paysIndex[element.country]={sdg_index:element.sdg_index_score}
+        }
+    });
+    
+
+    d3.select("#main").selectAll("path")
+    .style("fill",function (d) {
+        
+        if(paysIndex[d.properties.name]!=undefined)
+        {
+            
+           return sdgColorScale(paysIndex[d.properties.name].sdg_index)
+        }
+        else
+        {
+            return "black"
+        }
+    }
+         )
+
+};
