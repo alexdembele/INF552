4c249156d557d2a41acd375ec34ba3af7617a6cd
alexdembele <chessdembel@gmail.com>
2023-11-29 21:40:44 +0100

    pays cliquable

--- js/initialisation.js
+++ js/initialisation.js
@@ -5,6 +5,7 @@ const ctx = {
 
 function createViz() {
     console.log("Using D3 v" + d3.version);
+    
     // Création de la projection géographique
     var projection = d3.geoMercator()
     .scale(120)
@@ -18,15 +19,17 @@ function createViz() {
     .attr("height", ctx.height);
 
     // Charge les données géographiques
-    d3.json("data/world-110m.json").then(function (world) {
+    d3.json("data/country-110m.json").then(function (world) {
     // Ajoute les pays à la carte
+    console.log(topojson.feature(world, world.objects.countries).features)
     svg.selectAll("path")
     .data(topojson.feature(world, world.objects.countries).features)
     .enter().append("path")
     .attr("d", path)
-    .on("click", function (d) {
+    .on("click", function (event,d) {
         // Gestion de l'événement de clic
-        console.log("Pays cliqué :", d.properties.name);
+
+        console.log("Pays cliqué :", d.properties.name)
     });
 });
 
